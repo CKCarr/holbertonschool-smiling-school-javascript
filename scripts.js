c1101e6a7ab9858c426360f26c0fe15c8f3817d0
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-11-28 08:05:45 -0600

    this video section is not working but my quotes section is fine sending changes to keep working

--- scripts.js
+++ scripts.js
@@ -3,13 +3,13 @@
 $(document).ready(function() {
     /** Carousel QUOTES loader*/
     loadQuotes();
+    loadVideos();
 
-e
 });
 
 /** Carousel QUOTES loader*/
 function loadQuotes() {
-    const carouselInner = $('#carouselExampleControls .carousel-inner');
+    const carouselInner = $('#carouselExampleControls .carousel-inner .loadItems');
     $('.loader').show();
 
     $.ajax({
@@ -39,12 +39,71 @@ function loadQuotes() {
                 `);
                 carouselInner.append(carouselItem);
             });
-            // Reinitialize or update carousel
 
         },
         error: function(error) {
-            console.error('Error:', error);
+            console.error('Error: ', error);
             $('.loader').hide();
+        }
+    });
+}
+
+
+
+/** Carousel QUOTES loader*/
+function loadVideos() {
+    const carouselInner = $('#carouselExampleControls2 .carousel-inner .loadItems2');
+    $('.loader2').show();
+
+    $.ajax({
+        url: 'https://smileschool-api.hbtn.info/quotes',
+        type: 'GET',
+        success: function(quotes) {
+            $('.loader2').hide();
+            carouselInner.empty();
+
+            $.each(quotes, function(index, video) {
+                const isActive = index === 0 ? ' active' : '';
+                const videoCard = $(`
+                <div class="carousel-item active">
+                <div class="row align-items-center mx-auto">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center">
+                        <div class="card">
+                                        <img src="${video.thumb_url}" class="card-img-top" alt="${video.title}" />
+                                        <div class="card-img-overlay text-center">
+                                            <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
+                                        </div>
+                                        <div class="card-body">
+                                            <h5 class="card-title font-weight-bold">${video.title}</h5>
+                                            <p class="card-text text-muted">${video["sub-title"]}</p>
+                                            <div class="creator d-flex align-items-center">
+                                                <img src="${video.author_pic_url}" alt="${video.author}" width="30px" class="rounded-circle" />
+                                                <h6 class="pl-3 m-0 main-color">${video.author}</h6>
+                                            </div>
+                                            <div class="info pt-3 d-flex justify-content-between">
+                                                <div class="rating">${stars}</div>
+                                                <span class="main-color">${video.duration}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                `);
+                const carouselItem = $(`
+                        <div class="carousel-item${isActive}">
+                            <div class="row align-items-center mx-auto">
+                            </div>
+                        </div>
+                    `);
+                    carouselItem.append(videoCard);
+                carouselInner.append(carouselItem);
+            });
+
+        },
+        error: function(error) {
+            console.error('Error: ', error);
+            $('.loader2').hide();
         }
     });
 }
