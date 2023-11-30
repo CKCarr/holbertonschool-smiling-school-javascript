b98e4e493f09ceb9e690540e964687b529c4aa78
CKCarr <119520583+CKCarr@users.noreply.github.com>
2023-11-29 18:38:38 -0600

    closer to a video carousel - TODO refactor the card rows/cols in different viewpoints. whole row not added but add a column ata  time

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@
 $(document).ready(function() {
     /** Carousel QUOTES loader*/
     loadQuotes();
+    /** Carousel Video Loader */
     loadVideos();
 
 });
@@ -15,6 +16,7 @@ function loadQuotes() {
     $.ajax({
         url: 'https://smileschool-api.hbtn.info/quotes',
         type: 'GET',
+        dataType: 'json',
         success: function(quotes) {
             $('.loader').hide();
             carouselInner.empty();
@@ -50,60 +52,86 @@ function loadQuotes() {
 
 
 
-/** Carousel QUOTES loader*/
+/** Carousel VIDEO Loader */
 function loadVideos() {
-    const carouselInner = $('#carouselExampleControls2 .carousel-inner .loadItems2');
+    // Display the loader while data is being fetched
     $('.loader2').show();
 
+    // Make an AJAX request to fetch the JSON data
     $.ajax({
-        url: 'https://smileschool-api.hbtn.info/quotes',
-        type: 'GET',
-        success: function(quotes) {
-            $('.loader2').hide();
-            carouselInner.empty();
+        url: 'https://smileschool-api.hbtn.info/popular-tutorials',
+        method: 'GET',
+        dataType: 'json',
+        success: function (videoItem) {
+        // Hide the loader when data is loaded
+        $('.loader2').hide();
 
-            $.each(quotes, function(index, video) {
-                const isActive = index === 0 ? ' active' : '';
-                const videoCard = $(`
-                <div class="carousel-item active">
-                <div class="row align-items-center mx-auto">
-                    <div class="col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center">
-                        <div class="card">
-                                        <img src="${video.thumb_url}" class="card-img-top" alt="${video.title}" />
-                                        <div class="card-img-overlay text-center">
-                                            <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
-                                        </div>
-                                        <div class="card-body">
-                                            <h5 class="card-title font-weight-bold">${video.title}</h5>
-                                            <p class="card-text text-muted">${video["sub-title"]}</p>
-                                            <div class="creator d-flex align-items-center">
-                                                <img src="${video.author_pic_url}" alt="${video.author}" width="30px" class="rounded-circle" />
-                                                <h6 class="pl-3 m-0 main-color">${video.author}</h6>
-                                            </div>
-                                            <div class="info pt-3 d-flex justify-content-between">
-                                                <div class="rating">${stars}</div>
-                                                <span class="main-color">${video.duration}</span>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+        // Check if data is available and not empty
+        if (videoItem && videoItem.length > 0) {
+            // Get the carousel inner element
+            let carouselInner = $('#carouselExampleControls2 .carousel-inner .loadItems2');
+
+            // Create the row element for cards
+            let cardRow = $('<div>').addClass('row align-items-center mx-auto');
+
+            // Loop through the data items
+            $.each(videoItem, function (index, videoData) {
+                // Create a card element
+                let cardCol = $('<div>').addClass('col-12 col-sm-6 col-md-6 col-lg-3 d-flex justify-content-center justify-content-md-end justify-content-lg-center');
+
+                let card = `
+                    <div class="card">
+                    <img src="${videoData.thumb_url}" class="card-img-top" alt="Video thumbnail" />
+                    <div class="card-img-overlay text-center">
+                        <img src="images/play.png" alt="Play" width="64px" class="align-self-center play-overlay" />
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">${videoData.title}</h5>
+                        <p class="card-text text-muted">${videoData['sub-title']}</p>
+                        <div class="creator d-flex align-items-center">
+                        <img src="${videoData.author_pic_url}" alt="Creator of Video" width="30px" class="rounded-circle" />
+                        <h6 class="pl-3 m-0 main-color">${videoData.author}</h6>
                         </div>
-                `);
-                const carouselItem = $(`
-                        <div class="carousel-item${isActive}">
-                            <div class="row align-items-center mx-auto">
-                            </div>
+                        <div class="info pt-3 d-flex justify-content-between">
+                        <div class="rating">
+                            <!-- Add star rating here -->
                         </div>
-                    `);
-                    carouselItem.append(videoCard);
-                carouselInner.append(carouselItem);
+                        <span class="main-color">${videoData.duration}</span>
+                        </div>
+                    </div>
+                    </div>
+                `;
+                // Append the card to the column
+                cardCol.html(card);
+
+                // Append the card column element to the row
+                cardRow.append(cardCol);
+
+                // add the row to the carousel inner after every 4 cards
+                if ((index + 1 ) % 4 === 0) {
+                    let carouselItem = $('<div>').addClass('carousel-item');
+                    carouselItem.append(cardRow);
+                    carouselInner.append(carouselItem);
+
+                    // Reset the row element
+                    cardRow = $('<div>').addClass('row align-items-center mx-auto');
+                }
             });
+            // check if there are any remaining cards in the row and append them to the last carousel item
+            if (cardRow.children().length > 0) {
+                let carouselItem = $('<div>').addClass('carousel-item');
+                carouselItem.append(cardRow);
+                carouselInner.append(carouselItem);
+            }
+            // Activate the first carousel item
+            $('#carouselExampleControls2 .carousel-inner .carousel-item:first').addClass('active');
 
-        },
-        error: function(error) {
-            console.error('Error: ', error);
-            $('.loader2').hide();
+            // Activate the carousel
+            $('#carouselExampleControls2').carousel();
         }
+    },
+    error: function (error) {
+        console.log('Error fetching data:', error);
+        },
     });
 }
